95bda4d9587c4019c4920bb5cf61b40630c5419b
jgerega107 <jakegerega107@gmail.com>
2022-05-03 17:15:08 -0400

    Added login routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const playerRoutes = require("./players");
 const newsRoutes = require("./news");
 const homeRoutes = require("./home");
 const debugRoutes = require("./debug");
+const authRoutes = require("./auth");
 
 const constructorMethod = (app) => {
   //app.use("/", userRoutes);
@@ -11,6 +12,7 @@ const constructorMethod = (app) => {
   app.use("/", playerRoutes);
   app.use("/debug", debugRoutes); //temporary
   app.use("/", homeRoutes);
+  app.use("/", authRoutes);
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Not Found" });
   });
