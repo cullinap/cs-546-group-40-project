9527cda9153f65468ef069a8dc651977fd67986d
jgerega107 <jakegerega107@gmail.com>
2022-05-06 00:04:12 -0400

    Added work on profile updating

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,3 +1,58 @@
 (function ($) {
-    
+  const changePfpDiv = $("#myprofile-changepfp"),
+    changeUsnDiv = $("#myprofile-changeusername"),
+    changeEmDiv = $("#myprofile-changeemail"),
+    changePwdDiv = $("#myprofile-changepassword"),
+    pfpButton = $("#myprofile-pfpButton"),
+    usnButton = $("#myprofile-usnButton"),
+    emButton = $("#myprofile-emButton"),
+    pwdButton = $("#myprofile-pwdButton"),
+    pfpForm = $("#pfp-form"),
+    usnForm = $("#usn-form"),
+    emForm = $("#em-form"),
+    pwdForm = $("#pwd-form");
+
+  function clickUpdatePfp(event) {
+    event.preventDefault();
+    hideAll();
+    changePfpDiv.removeAttr("hidden");
+  }
+  function clickUpdateUsn(event) {
+    event.preventDefault();
+    hideAll();
+    changeUsnDiv.removeAttr("hidden");
+  }
+  function clickUpdateEmail(event) {
+    event.preventDefault();
+    hideAll();
+    changeEmDiv.removeAttr("hidden");
+  }
+  function clickUpdatePwd(event) {
+    event.preventDefault();
+    hideAll();
+    changePwdDiv.removeAttr("hidden");
+  }
+
+  function hideAll() {
+    changePfpDiv.attr("hidden", true);
+    changeUsnDiv.attr("hidden", true);
+    changeEmDiv.attr("hidden", true);
+    changePwdDiv.attr("hidden", true);
+  }
+
+  pfpButton.bind("click", clickUpdatePfp);
+  usnButton.bind("click", clickUpdateUsn);
+  emButton.bind("click", clickUpdateEmail);
+  pwdButton.bind("click", clickUpdatePwd);
+
+  pfpForm.bind("submit", (event) => {
+    event.preventDefault();
+    let url = $("#pfpurl").val();
+    if (!url) {
+      alert("No URL was specified.");
+    }
+    $.post("/myprofile/changepfp", { pfpurl: url }, function (receivedData) {
+      alert(receivedData);
+    });
+  });
 })(window.jQuery);
--- routes/profile.js
+++ routes/profile.js
@@ -10,11 +10,64 @@ router.get("/myprofile", async (req, res) => {
   let { username, email, profilePicture } = await userData.getUser(
     req.session.user.email
   );
-  res.render("profile/profile", {
+  res.render("profile", {
     username: username,
     email: email,
     pfp: profilePicture,
   });
+});
+
+router.post("/myprofile/changepfp", async (req, res) => {
+  if (!req.session.user) {
+    res.status(403);
+    return;
+  }
+  const email = req.session.user.email;
+  const pfpUrl = req.body.pfpurl;
+  if (!email) {
+    res.status(400).send("Email is a required field");
+    return;
+  }
+  if (!pfpUrl) {
+    res.status(400).send("Profile picture URL is a required field");
+    return;
+  }
+  if (typeof email != "string") {
+    res.status(400).send("Email must be a string");
+    return;
+  }
+  if (typeof pfpUrl != "string") {
+    res.status(400).send("Profile picture URL is a string");
+    return;
+  }
+  let response = await userData.updateUserPfp(email, pfpUrl);
+  if (response) {
+    res.status(200).send("Successfully changed profile picture.");
+  } else {
+    res.status(500).send("Failed to change profile picture.");
+  }
+  return;
+});
+
+router.post("/myprofile/changeusn", async (req, res) => {
+  if (!req.session.user) {
+    res.status(403);
+    return;
+  }
+});
+
+router.post("/myprofile/changepwd", async (req, res) => {
+  if (!req.session.user) {
+    res.status(403);
+    return;
+  }
+});
+
+router.post("/myprofile/changeem", async (req, res) => {
+  if (!req.session.user) {
+    res.status(403);
+    return;
+  }
 });
 
 module.exports = router;
