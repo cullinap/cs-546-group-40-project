6d337d0e6d3221ea1a3e747f575dd659f7ba379d
jgerega107 <jakegerega107@gmail.com>
2022-05-06 01:47:06 -0400

    Removed obsolete data files

--- data/index.js
+++ data/index.js
@@ -2,14 +2,10 @@ const userData = require("./user");
 const playerData = require("./players");
 const discussionData = require("./discussion");
 const userTeamData = require("./user_team");
-const homeData = require("./home");
-const newsData = require("./news")
 
 module.exports = {
   userData,
   playerData,
   discussionData,
   userTeamData,
-  homeData,
-  newsData
 };
