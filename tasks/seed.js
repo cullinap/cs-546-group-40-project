4521507686f3f3268ffc509dc3fcb8ece9aeb1d4
cullinap <cullinap@gmail.com>
2022-05-07 12:06:35 -0400

    added seed script

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,35 +1,41 @@
-const userData = require("../data/user");
-const playerData = require("../data/players");
 const connection = require("../config/mongoConnection");
 const mongoCollections = require("../config/mongoCollections");
-const { homeData } = require("../data");
+const { userData } = require("../data");
 const axios = require("axios");
+const passwordHash = require('password-hash');
 
-const playerIDMaps = 
-    "https://raw.githubusercontent.com/cullinap/data_sources/main/player_id_map.json"
+async function userSeed() {
+    const db = await connection.connectToDb();
+    await db.dropDatabase();
+    console.log("seeding users") 
 
+    let email_one = 'firstuser@gmail.com';
+    let password_one = passwordHash.generate('123456')
 
-async function playerSeed() {
-    // const db = await connection.connectToDb();
-    // await db.dropDatabase();
-    console.log("creating player") 
+    let email_two = 'seconduser@yahoo.com';
+    let password_two = passwordHash.generate('password')
 
-    let playerData = await homeData.playerNameIdMap();
-    // try {
-    //     await mapEspnIdToPlayer.(
-    //         id: 
-    //         , firstName
-    //         , lastName
-    //     )
+    try {
+        await userData.createUser(
+            email_one
+            , password_one
+        )
+    } catch(e) {
+        console.log(e)
+    }
 
-    // console.log('player created')
+    try {
+        await userData.createUser(
+            email_two
+            , password_two
+        )
+    } catch(e) {
+        console.log(e)
+    }
 
-    // } catch(e) {
-    //     console.log(e)
-    // }
-
-    // await connection.closeConnection();
+    console.log('user(s) created')
+    await connection.closeConnection();
 }
 
-playerSeed()
+userSeed()
 
