4a21f476530d93b3a0289b1cabd3e32932726ca3
jgerega107 <jakegerega107@gmail.com>
2022-05-08 07:43:33 -0400

    Added ESPN and NBC clips

--- data/videos.js
+++ data/videos.js
@@ -2,6 +2,7 @@ const gooogleapikey = "AIzaSyDC_mlAVWNslRgx8N37JYVeS3pWcYcG1jc";
 const axios = require("axios");
 
 const espnclips = `https://www.googleapis.com/youtube/v3/search?&key=${gooogleapikey}&channelId=UCiWLfSweyRNmLpgEHekhoAg&part=snippet,id&order=date&maxResults=20`;
+const nbcclips = `https://www.googleapis.com/youtube/v3/search?&key=${gooogleapikey}&channelId=UCqZQlzSHbVJrwrn5XvzrzcA&part=snippet,id&order=date&maxResults=20`;
 
 async function getApiData(url) {
   let { data } = await axios.get(url);
@@ -13,6 +14,12 @@ async function getEspnClips() {
   return items;
 }
 
+async function getNbcClips() {
+  let {items} = await getApiData(nbcclips);
+  return items;
+}
+
 module.exports = {
   getEspnClips,
+  getNbcClips,
 };
--- routes/videos.js
+++ routes/videos.js
@@ -4,10 +4,12 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   let espn = await vidData.getEspnClips();
+  let nbc = await vidData.getNbcClips();
   try {
     res.render("clips", {
       title: "Recent Clips",
       espn: espn,
+      nbc: nbc,
     });
   } catch (e) {
     res.status(500).json({ error: e });
